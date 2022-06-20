34557113f8c9b593e727f144ce7f0491f1ac7e11
Luciano David Dorregaray <lucianodavid2201@gmail.com>
2022-06-20 17:09:55 -0300

    Countries added

--- henry/src/Components/Register_form/Students/.anguages.jsx
+++ henry/src/Components/Register_form/Students/.anguages.jsx
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
+import { allCountries } from "./paises";
 
 export default function Languages () {
     const [ country, setCountry ] = useState({
         country:"",
         city:""
     });
+
+    return (
+        <div>
+            <div><h3>¿De qué país eres?</h3></div>
+            <select>
+                { allCountries.map(e => <option key={e}>{e}</option>) }
+            </select>
+        </div>
+    );
 };
