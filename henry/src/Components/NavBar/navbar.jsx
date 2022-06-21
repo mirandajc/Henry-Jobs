67ae9e2cd6c204f5ee987c14f3fb4e6ee6cd14e9
ReneCadena <renecadenacontacto@gmail.com>
2022-06-21 13:34:48 -0300

    moked files

--- henry/src/Components/NavBar/navbar.jsx
+++ henry/src/Components/NavBar/navbar.jsx
@@ -4,24 +4,32 @@ import Logo from "./Logo/logo";
 import ButtonLogin from "./buttons/buttonLogin";
 import ButtonAbout from "./buttons/buttonAbout";
 import LoginApi from "./buttons/LoginApi";
-import { NavbarS,NavButton} from "./NavStyles/navbar";
+import { NavbarS, NavButton } from "./NavStyles/navbar";
 import ButtonHome from './buttons/buttonHome'
 import ButtonLanding from './buttons/buttonLanding'
 
-export default function Navbar(){
-    return(
+export default function Navbar() {
+    return (
         <NavbarS>
+
             <div>
-            <Logo/>
+                <Logo />
             </div>
+
             <NavButton>
-            {/* <ButtonContact/>
+                {/* <ButtonContact/>
             <ButtonAbout/> */}
-            <ButtonLogin/>
-            <ButtonHome/>
-            <LoginApi/>
+
+                <ButtonLogin />
+
+                <ButtonHome />
+
+                <LoginApi />
+
             </NavButton>
-            <ButtonLanding/>
+
+            <ButtonLanding />
+
         </NavbarS>
     )
 }
