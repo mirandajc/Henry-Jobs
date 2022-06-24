55f0fddcaee296ee406d88f227ceb6c8f8f1b3cc
nahuel cempellin <nahuelcempellin@gmail.com>
2022-06-24 10:45:35 -0300

    finalizado form alumnos

--- henry/src/Components/Register_form/RegisterCompleted.jsx
+++ henry/src/Components/Register_form/RegisterCompleted.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { CompleteStudentCont } from "./formStyles/completeStudent";
 
 export default function RegisterCompleted () {
 
     // CUANDO TERMINA EL REGISTRO, SE HACE UN AUTOLOGIN ? O EL USUARIO TIENE QUE LOGEARSE ?
     // EL CODIGO DE ABAJO ESTA HECHO COMO UN AUTOLOGIN
     
-    return <div>
+    return <CompleteStudentCont>
         <h1>Te has registrado exitosamente!</h1>
-        <h3>ya puedes <br /> ir a <Link to={'/home'}>Home</Link> a ver las publicaciones <br /> ir a tu perfil</h3>
-    </div>
+        <h1>Bienvenido!!! 🎉🎊🎉</h1>
+        
+        <h2>Puedes ir a <Link to={'/home'}>Home</Link>, a ver las publicaciones o ingresar a tu perfil.</h2>
+        
+    </CompleteStudentCont>
 };
