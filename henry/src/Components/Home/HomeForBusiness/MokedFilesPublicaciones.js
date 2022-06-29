659630ffd3affcc3174aa82a3e9f7091cd7f73a3
ReneEnriqueCadena <renecadenacontacto@gmail.com>
2022-06-28 21:27:09 -0300

    borre todo el reducer

--- henry/src/Components/Home/HomeForBusiness/MokedFilesPublicaciones.js
+++ henry/src/Components/Home/HomeForBusiness/MokedFilesPublicaciones.js
@@ -1,14 +1,38 @@
 export const mokedFilesPublicaciones = [
 
-    { id: 7, name: 'Laura Ana', date: '12-10-18', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
-    { id: 4, name: 'Luciano Dorregaray', date: '08-09-19', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
-    { id: 2, name: 'Nahuel Cempellini', date: '12-10-20', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
-    { id: 3, name: 'Tomas Arregui', date: '11-10-21', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
-    { id: 1, name: 'Jocelyn Miranda', date: '12-10-21', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
-    { id: 5, name: 'Rene Cadena', date: '03-12-21', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
-    { id: 6, name: 'Gabriel Cavasso', date: '06-06-22', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
+    {userTypes: 1, id: 7, name: 'Laura Ana', date: '12-10-18', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
+    {userTypes: 2, id: 4, name: 'Luciano Dorregaray', date: '08-09-19', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
+    {userTypes: 1, id: 2, name: 'Nahuel Cempellini', date: '12-10-20', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
+    {userTypes: 2, id: 3, name: 'Tomas Arregui', date: '11-10-21', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
+    {userTypes: 1, id: 1, name: 'Jocelyn Miranda', date: '12-10-21', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
+    {userTypes: 2, id: 5, name: 'Rene Cadena', date: '03-12-21', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
+    {userTypes: 1, id: 6, name: 'Gabriel Cavasso', date: '06-06-22', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
+
+]
+
+export const mokedFilesPostBusiness = [
+
+    {userTypes: 5,id: 9, backFront: 'Frontend', english: 'si' ,workModality: 'Remoto', ubication: 'Argentina' ,technologies: ['React', 'Java'] ,posterUser: 'MessiElKing', text: 'esto es una propuesta laboral', img: '',},
+    {userTypes: 5,id: 10, backFront: 'Fullstack', english: 'no' ,workModality: 'Presencial', ubication: 'Brazil' ,technologies: ['Golan', 'Java'] ,posterUser: 'MessiElKing', text: 'esto es una propuesta laboral 2', img: '',},
+    {userTypes: 5,id: 11, backFront: 'Backend', english: 'no' ,workModality: 'Remoto', ubication: 'Colombia' ,technologies: ['Sql', 'Mongo'] ,posterUser: 'MessiElKing', text: 'esto es una propuesta laboral 3', img: '',},
+    {userTypes: 5,id: 12, backFront: 'Frontend', english: 'si' ,workModality: 'Presencial', ubication: 'Peru' ,technologies: ['React', 'Redux'] ,posterUser: 'MessiElKing', text: 'esto es una propuesta laboral 4', img: '',},
+    {userTypes: 1, id: 7, name: 'Laura Ana', date: '12-10-18', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
+    {userTypes: 2, id: 4, name: 'Luciano Dorregaray', date: '08-09-19', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
+    {userTypes: 1, id: 2, name: 'Nahuel Cempellini', date: '12-10-20', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
+    {userTypes: 2, id: 3, name: 'Tomas Arregui', date: '11-10-21', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
+    {userTypes: 1, id: 1, name: 'Jocelyn Miranda', date: '12-10-21', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
+    {userTypes: 2, id: 5, name: 'Rene Cadena', date: '03-12-21', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
+    {userTypes: 1, id: 6, name: 'Gabriel Cavasso', date: '06-06-22', picture: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Icono_Usuario.png', proyectTittle: 'Primer Proyecto', summary: 'este va a ser mi primer proyecto', imgVideo: 'https://www.youtube.com/watch?v=3PHXvlpOkf4', linkedin: 'www.linkedin.com', technologies: ['golang', 'js'] },
+]
+
+export const mokedFilesStudens = [
+    {userTypes: 1, name: 'Rene', lastName: 'Cadena', english: 'C1', cuntry: 'Argentina', city: 'Mendoza', technologies: ['Java','React'], backFront: 'Frontend',picture: ''},
+    {userTypes: 2, name: 'Nahu', lastName: 'Crack', english: 'A0', cuntry: 'Peru', city: 'Lima', technologies: ['Java','React'], backFront: 'Backend',picture: ''},
+    {userTypes: 1, name: 'Lucho', lastName: 'Nashe', english: 'A1', cuntry: 'Chile', city: 'Santiago', technologies: ['Java','Redux'], backFront: 'Backend',picture: ''},
+    {userTypes: 2, name: 'Joce', lastName: 'lyn', english: 'A2', cuntry: 'Mexico', city: 'Acapulco', technologies: ['Java','React'], backFront: 'Fullstack',picture: ''},
+    {userTypes: 1, name: 'Tomi', lastName: 'Gabi', english: 'C2', cuntry: 'Argentina', city: 'Mendoza', technologies: ['Golang','React'], backFront: 'Frontend',picture: ''}
 
 ]
 
 
-console.log(mokedFilesPublicaciones.sort((a, b) => a.date > b.date));
+/* console.log(mokedFilesPublicaciones.sort((a, b) => a.date > b.date)); */
--- henry/src/Components/Home/HomeForStudents/HomeStudentsLogic.jsx
+++ henry/src/Components/Home/HomeForStudents/HomeStudentsLogic.jsx
@@ -5,21 +5,14 @@ import { usePagination } from 'use-pagination-hook';
 
 export default function HomeStudentsLogic() {
 
-    const { current, pages, display, next, previous } = usePagination({ items: 'hola' , size: 2 }); //hola reemplazar por el array a renderizar
+
 
     return (
         <div>
             
+            
 
-            <p>Page {current} of {pages}</p>
-            <ul>
-                {
-                    display.map(e => <HomeStudentsCard />)
-                }
-            </ul>
-            <button disabled={current === 1} onClick={previous}>Previous Page</button>
-            <button disabled={current === pages} onClick={next}>Next Page</button>
-            <p>{current}</p>
+            
 
         </div>
     )
