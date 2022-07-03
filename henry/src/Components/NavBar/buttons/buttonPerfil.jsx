99627278f817c99f9519477b4381fb88e63e2286
ReneEnriqueCadena <renecadenacontacto@gmail.com>
2022-07-03 15:20:06 -0300

    lucho gato

--- henry/src/Components/NavBar/buttons/buttonPerfil.jsx
+++ henry/src/Components/NavBar/buttons/buttonPerfil.jsx
@@ -1,41 +1,24 @@
 import React, { useEffect, useState } from "react";
 import {useDispatch, useSelector} from 'react-redux'; 
-import {useNavigate} from 'react-router-dom';
 import { Link } from "react-router-dom";
 import { useJwt } from "react-jwt";
 import { profileID } from "../../../reducer/actions/actionPost";
 
 export default function ButtonPerfil(props){
-    const navigate= useNavigate();
+    
     const dispatch= useDispatch();
-    const user= useSelector((state)=> state.fetchPostReducer.response);
-    
-   /*  const dispatch= useDispatch();
-    const userID= useSelector((state)=> state.fetchPostSaga.response);
-    console.log(userID) */
-
-    
-
+    const user = useSelector((state)=> state.fetchPostReducer.response);
     const { decodedToken, isExpided } = useJwt(user);
     const respuesta = decodedToken;
     const[id, setId]=useState('')
 
-
-    // const token= localStorage.getItem('TK');
-    // const parseTK= JSON.parse(token)
-    // console.log(parseTK)
-    
-    
     useEffect(()=>{
-        
         if(respuesta !== null) setId(respuesta.id);
-       
-       
     },[respuesta])
     
-        function handleID(){
-            dispatch(profileID(id))
-        }
+    const handleID = function(){
+        dispatch(profileID(id))
+    }
 
     return(
         <div>
--- henry/src/Components/Profile/Students/studentProfileComplete.jsx
+++ henry/src/Components/Profile/Students/studentProfileComplete.jsx
@@ -1,43 +1,46 @@
 import React, { useEffect, useState } from "react";
-import {useDispatch, useSelector} from 'react-redux';
-import {useParams} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import StudentsProfile from "./students_profile";
 import StudentCV from "./students-cv";
 import { CompleteProfile } from "../profileStyles/StudentComplete";
 import { profileID } from "../../../reducer/actions/actionPost";
 
-export default function StudentCompletePage(){
+export default function StudentCompletePage() {
 
-    const{id}= useParams();
-    console.log(id)
-    const dispatch=useDispatch();
-    const userProfile= useSelector((state)=> state.fetchPostReducer.responseProfile)
-    const [user,setUser]= useState('');
-   
-   
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    const userProfile = useSelector((state) => state.fetchPostReducer.responseProfile)
+    const [renderi, setRenderi] = useState(false);
+
     console.log(userProfile)
 
-   
-
-    useEffect(()=>{
-
-        if(userProfile !== null){ 
-            setUser(userProfile);
+    useEffect(() => {
+        if(userProfile.name !== undefined) {
+            setRenderi(true)
         }
-       
-    },[userProfile])
+    }, [userProfile])
 
- 
 
-return(
-    <CompleteProfile>
+    return (
+        <CompleteProfile>
 
-        <StudentsProfile/>
-        <StudentCV technologies={userProfile.technologies}
-        otherStudies={userProfile.otherStudies}/>
+            {
+                renderi
+                    ?
+                    <div>
+                        <StudentCV
+                            technologies={userProfile.technologies}
+                            otherStudies={userProfile.otherStudies}
+                        />
+                        <StudentsProfile />
+                    </div>
 
-    </CompleteProfile>
-)
+                    : null
+            }
+
+        </CompleteProfile>
+    )
 
 
 }
