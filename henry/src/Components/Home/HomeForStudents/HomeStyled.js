17613ba0d43a238fffdcadacd7cfe64f8cd5ede8
nahuel cempellin <nahuelcempellin@gmail.com>
2022-07-03 22:31:08 -0300

    cambios en el perfil0
    asd

--- henry/src/Components/Home/HomeForStudents/HomeStyled.js
+++ henry/src/Components/Home/HomeForStudents/HomeStyled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const ContenedorStudents = styled.div`
     width: 100%;
+    height:auto;
     display: flex;
     justify-content: center;
     flex-direction: column;
